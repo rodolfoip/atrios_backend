2d1dbc151fb3338a20cdef9d8b12a0aa734b07df
rodolfoip <rodolfo.pereirailce@gmail.com>
2021-06-08 07:33:45 -0300

    test: ensure LoadUserByEmailRepository returns null if no user is found

---
+++ src/infra/repositories/load-user-by-email-repository.spec.js
@@ -0,0 +1,12 @@
+class LoadUserByEmailRepository {
+  async load (email) {
+    return null
+  }
+}
+describe('LoadUserByEmailRepository', () => {
+  it('should return null if no user is found', async () => {
+    const sut = new LoadUserByEmailRepository()
+    const user = await sut.load('invalid_email@mail.com')
+    expect(user).toBeNull()
+  })
+})
