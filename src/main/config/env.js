1784a14c3c59e35834d724c231f49483e86a4c58
rodolfoip <rodolfo.pereirailce@gmail.com>
2021-06-17 08:43:22 -0300

    refactor: move loginRouter composition to its own file

--- src/main/config/env.js
+++ src/main/config/env.js
@@ -1,3 +1,4 @@
 module.exports = {
-  mongoUrl: process.env.MONGO_URL || 'mongodb://localhost:xxx/atrios-node-api'
+  mongoUrl: process.env.MONGO_URL || 'mongodb://localhost:xxx/atrios-node-api',
+  tokenSecret: process.env.TOKEN_SECRET || 'secret'
 }
