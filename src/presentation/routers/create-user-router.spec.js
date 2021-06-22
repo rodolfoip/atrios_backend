1771f41f2197542d4109264e82734c563a5c8b2b
rodolfoip <rodolfo.pereirailce@gmail.com>
2021-06-21 21:42:22 -0300

    test: ensure CreateUserRouter throws if invalid params are provided

---
+++ src/presentation/routers/create-user-router.spec.js
@@ -0,0 +1,82 @@
+const { MissingParamError } = require('../../utils/errors')
+const { ServerError } = require('../errors')
+const HttpResponse = require('../helpers/http-response')
+
+class CreateUserRoute {
+  route (httpRequest) {
+    try {
+      const { name, email, password } = httpRequest.body
+      if (!name) {
+        return HttpResponse.badRequest(new MissingParamError('name'))
+      }
+      if (!email) {
+        return HttpResponse.badRequest(new MissingParamError('email'))
+      }
+      if (!password) {
+        return HttpResponse.badRequest(new MissingParamError('password'))
+      }
+    } catch (error) {
+      return HttpResponse.serverError()
+    }
+  }
+}
+
+const makeSut = () => {
+  const sut = new CreateUserRoute()
+
+  return { sut }
+}
+describe('CreateUser router', () => {
+  test('should return 400 if no name is provided', async () => {
+    const { sut } = makeSut()
+    const httpRequest = {
+      body: {
+        email: 'any_email@test.com',
+        password: '1234'
+      }
+    }
+    const httpResponse = await sut.route(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body.error).toBe(new MissingParamError('name').message)
+  })
+
+  test('should return 400 if no email is provided', async () => {
+    const { sut } = makeSut()
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        password: '1234'
+      }
+    }
+    const httpResponse = await sut.route(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body.error).toBe(new MissingParamError('email').message)
+  })
+
+  test('should return 400 if no password is provided', async () => {
+    const { sut } = makeSut()
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        email: 'any_email@test.com'
+      }
+    }
+    const httpResponse = await sut.route(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body.error).toBe(new MissingParamError('password').message)
+  })
+
+  test('should return 500 if no httpRequest is provided', async () => {
+    const { sut } = makeSut()
+    const httpResponse = await sut.route()
+    expect(httpResponse.statusCode).toBe(500)
+    expect(httpResponse.body.error).toBe(new ServerError().message)
+  })
+
+  test('should return 500 if httpRequest has no body', async () => {
+    const { sut } = makeSut()
+    const httpResponse = await sut.route({})
+    expect(httpResponse.statusCode).toBe(500)
+    expect(httpResponse.body.error).toBe(new ServerError().message)
+  })
+})
