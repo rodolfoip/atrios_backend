331483d4cb3371f4b294d65192605ecab93bd1b8
rodolfoip <rodolfo.pereirailce@gmail.com>
2021-06-25 23:49:53 -0300

    feat: added encrypter on composer

--- src/main/composers/create-user-router.composer.js
+++ src/main/composers/create-user-router.composer.js
@@ -5,6 +5,7 @@ const UserRepository = require('../../infra/repositories/user-repository')
 const CreateUserRoute = require('../../presentation/routers/create-user-router')
 const EmailValidator = require('../../utils/helpers/email-validator')
 const TokenGenerator = require('../../utils/helpers/token-generator')
+const Encrypter = require('../../utils/helpers/encrypter')
 const env = require('../config/env')
 
 module.exports = class CreateUserRouterComposer {
@@ -12,12 +13,14 @@ module.exports = class CreateUserRouterComposer {
     const tokenGenerator = new TokenGenerator(env.tokenSecret)
     const loadUserByEmailRepository = new LoadUserByEmailRepository()
     const updateAccessTokenRepository = new UpdateAccessTokenRepository()
+    const encrypter = new Encrypter()
     const userRepository = new UserRepository()
     const emailValidator = new EmailValidator()
     const createUseCase = new CreateUseCase({
       loadUserByEmailRepository,
       updateAccessTokenRepository,
       tokenGenerator,
+      encrypter,
       userRepository
     })
 
