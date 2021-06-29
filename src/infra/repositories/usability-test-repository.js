e80e5a60fb6c9371710055a98619319ef6a06d14
rodolfoip <rodolfo.pereirailce@gmail.com>
2021-06-29 18:31:56 -0300

    test: ensure UsabilityTestRepository persist test

---
+++ src/infra/repositories/usability-test-repository.js
@@ -0,0 +1,24 @@
+const { MissingParamError } = require('../../utils/errors')
+const MongoHelper = require('../helpers/mongo-helper')
+
+module.exports = class UsabilityTestRepository {
+  async persist (usabilityTestEntity) {
+    const { name, accessCode, prototypeLink, externalLink } = usabilityTestEntity
+    if (!name) {
+      throw new MissingParamError('name')
+    }
+    if (!accessCode) {
+      throw new MissingParamError('accessCode')
+    }
+    if (!prototypeLink) {
+      throw new MissingParamError('prototypeLink')
+    }
+    if (!externalLink) {
+      throw new MissingParamError('externalLink')
+    }
+
+    const usabilityTestModel = await MongoHelper.getCollection('usability_test')
+    const usabilityTest = await usabilityTestModel.insertOne({ name, accessCode, prototypeLink, externalLink })
+    return usabilityTest.ops[0]
+  }
+}
---
+++ src/infra/repositories/usability-test-repository.spec.js
@@ -0,0 +1,68 @@
+const { MissingParamError } = require('../../utils/errors')
+const MongoHelper = require('../helpers/mongo-helper')
+const UsabilityTestRepository = require('./usability-test-repository')
+let usabilityTestModel
+
+const makeSut = () => {
+  return new UsabilityTestRepository()
+}
+
+describe('Usability test repository', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL)
+    usabilityTestModel = await MongoHelper.getCollection('usability_test')
+  })
+
+  beforeEach(async () => {
+    await usabilityTestModel.deleteMany()
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  test('should throw if no name is provided', async () => {
+    const sut = makeSut()
+    const promise = sut.persist({})
+    expect(promise).rejects.toThrow(new MissingParamError('name'))
+  })
+
+  test('should throw if no accessCode is provided', async () => {
+    const sut = makeSut()
+    const promise = sut.persist({
+      name: 'any_testname'
+    })
+    expect(promise).rejects.toThrow(new MissingParamError('accessCode'))
+  })
+
+  test('should throw if no prototypeLink is provided', async () => {
+    const sut = makeSut()
+    const promise = sut.persist({
+      name: 'any_testname',
+      accessCode: 'any_code'
+    })
+    expect(promise).rejects.toThrow(new MissingParamError('prototypeLink'))
+  })
+
+  test('should throw if no externalLink is provided', async () => {
+    const sut = makeSut()
+    const promise = sut.persist({
+      name: 'any_testname',
+      accessCode: 'any_code',
+      prototypeLink: 'any_link'
+    })
+    expect(promise).rejects.toThrow(new MissingParamError('externalLink'))
+  })
+
+  test('should persist and return new usability test', async () => {
+    const sut = makeSut()
+    const fakeUsabilityTest = {
+      name: 'any_test',
+      accessCode: 'any_accessCode',
+      prototypeLink: 'any_prototypeLink',
+      externalLink: 'any_externalLink'
+    }
+    const persistedUsabilityTest = await sut.persist(fakeUsabilityTest)
+    expect(persistedUsabilityTest).toMatchObject(fakeUsabilityTest)
+  })
+})
