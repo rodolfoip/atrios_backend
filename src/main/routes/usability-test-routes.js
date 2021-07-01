9b25bd4f9f38a6eb6fd68367554dd8f9cb0e0c06
rodolfoip <rodolfo.pereirailce@gmail.com>
2021-06-30 21:00:01 -0300

    test: ensure route get to find usability tests

--- src/main/routes/usability-test-routes.js
+++ src/main/routes/usability-test-routes.js
@@ -1,8 +1,10 @@
 const { adapt } = require('../adapters/express-router-adapter')
 const CreateUsabilityTestComposer = require('../composers/create-usability-test.composer')
+const FindUserRouterComposer = require('../composers/find-all-usability-test.composer')
 const DeleteUsabilityTestComposer = require('../composers/remove-usability-test.composer')
 
 module.exports = router => {
   router.post('/usability-test', (adapt(CreateUsabilityTestComposer.compose())))
+  router.get('/usability-test', (adapt(FindUserRouterComposer.compose())))
   router.delete('/usability-test', (adapt(DeleteUsabilityTestComposer.compose())))
 }
