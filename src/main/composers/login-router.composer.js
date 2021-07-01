952b077a669283fec0019667fdba82dc7c1ba674
rodolfoip <rodolfo.pereirailce@gmail.com>
2021-07-01 08:21:03 -0300

    refactor: move auth use case files to path auth

--- src/main/composers/login-router.composer.js
+++ src/main/composers/login-router.composer.js
@@ -1,5 +1,5 @@
 const LoginRouter = require('../../presentation/routers/login-router')
-const AuthUseCase = require('../../domain/usecases/auth-usecase')
+const AuthUseCase = require('../../domain/usecases/auth/auth-usecase')
 const EmailValidator = require('../../utils/helpers/email-validator')
 const LoadUserByEmailRepository = require('../../infra/repositories/load-user-by-email-repository')
 const UpdateAccessTokenRepository = require('../../infra/repositories/update-access-token-repository')
