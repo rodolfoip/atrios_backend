5c9563ba8dd2a52973cd074a27135c90b6fc0c7d
rodolfoip <rodolfo.pereirailce@gmail.com>
2021-07-01 23:16:29 -0300

    feat: added find by name usability test routes

--- src/main/routes/usability-test-routes.js
+++ src/main/routes/usability-test-routes.js
@@ -1,12 +1,14 @@
 const { adapt } = require('../adapters/express-router-adapter')
 const CreateUsabilityTestComposer = require('../composers/create-usability-test.composer')
 const FindUserRouterComposer = require('../composers/find-usability-test.composer')
+const FindByNameUserRouterComposer = require('../composers/find-by-name-usability-test.composer')
 const DeleteUsabilityTestComposer = require('../composers/remove-usability-test.composer')
 const UpdateUserRouterComposer = require('../composers/update-usability-test.composer')
 
 module.exports = router => {
   router.post('/usability-test', (adapt(CreateUsabilityTestComposer.compose())))
   router.get('/usability-test', (adapt(FindUserRouterComposer.compose())))
+  router.get('/usability-test/{name}', (adapt(FindByNameUserRouterComposer.compose())))
   router.put('/usability-test', (adapt(UpdateUserRouterComposer.compose())))
   router.delete('/usability-test', (adapt(DeleteUsabilityTestComposer.compose())))
 }
--- src/main/routes/usability-test-routes.test.js
+++ src/main/routes/usability-test-routes.test.js
@@ -79,4 +79,17 @@ describe('Usability Routes', () => {
       .send(fakeUsabilityTest)
       .expect(204)
   })
+
+  test('should return 200 when valid params are provided', async () => {
+    await usabilityTestModel.insertOne({
+      name: 'any_test',
+      accessCode: 'any_accessCode',
+      prototypeLink: 'any_prototypeLink',
+      externalLink: 'any_externalLink'
+    })
+    await request(app)
+      .get('/api/usability-test')
+      .query({ name: 'any_test' })
+      .expect(200)
+  })
 })
