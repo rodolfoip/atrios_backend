190857b11ff066190d3b290e628a388a6167311e
rodolfoip <rodolfo.pereirailce@gmail.com>
2021-07-02 07:45:29 -0300

    feat: added params to httpRequest

--- src/main/adapters/express-router-adapter.js
+++ src/main/adapters/express-router-adapter.js
@@ -3,7 +3,8 @@ module.exports = class ExpressRouterAdapter {
   static adapt (router) {
     return async (req, res) => {
       const httpRequest = {
-        body: req.body
+        body: req.body,
+        params: req.params
       }
       const httpResponse = await router.route(httpRequest)
       res.status(httpResponse.statusCode).json(httpResponse.body)
