e270fa53381c922696e399d17eb396203e0ac095
rodolfoip <rodolfo.pereirailce@gmail.com>
2021-07-01 22:59:48 -0300

    refactor: export find by name use case

--- src/domain/usecases/usability-test/index.js
+++ src/domain/usecases/usability-test/index.js
@@ -1,11 +1,13 @@
 const CreateUseCase = require('./create-usability-test')
 const UpdateUseCase = require('./update-usability-test')
 const FindUseCase = require('./find-usability-test')
+const FindByNameUseCase = require('./find-by-name-usability-test')
 const DeleteUseCase = require('./delete-usability-test')
 
 module.exports = {
   CreateUseCase,
   UpdateUseCase,
   FindUseCase,
+  FindByNameUseCase,
   DeleteUseCase
 }
