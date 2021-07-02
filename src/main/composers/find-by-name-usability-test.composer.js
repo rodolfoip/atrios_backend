a77db11e7218f882bf836bbc0b34b629d810f96c
rodolfoip <rodolfo.pereirailce@gmail.com>
2021-07-02 07:54:26 -0300

    fix: using correct usecase

--- src/main/composers/find-by-name-usability-test.composer.js
+++ src/main/composers/find-by-name-usability-test.composer.js
@@ -1,16 +1,16 @@
-const { FindUseCase } = require('../../domain/usecases/usability-test')
+const { FindByNameUseCase } = require('../../domain/usecases/usability-test')
 const UsabilityTestRepository = require('../../infra/repositories/usability-test-repository')
 const { FindByNameUsabilityTestRouter } = require('../../presentation/routers/usability-test')
 
 module.exports = class FindUserRouterComposer {
   static compose () {
     const usabilityTestRepository = new UsabilityTestRepository()
-    const findUseCase = new FindUseCase({
+    const findByNameUseCase = new FindByNameUseCase({
       usabilityTestRepository
     })
 
     return new FindByNameUsabilityTestRouter({
-      findUseCase
+      findByNameUseCase
     })
   }
 }
