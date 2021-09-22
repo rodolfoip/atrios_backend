05ba1e6dfe87fb24f018cfa47e53982b0b6934c6
rodolfoip <rodolfo.pereirailce@gmail.com>
2021-09-22 08:03:37 -0300

    feat: added quantity property to test

--- src/domain/models/usability-test.js
+++ src/domain/models/usability-test.js
@@ -1,10 +1,11 @@
 module.exports = class UsabilityTest {
-  constructor (_id, name, accessCode, prototypeLink, externalLink, tasks = []) {
+  constructor (_id, name, accessCode, prototypeLink, externalLink, tasks = [], quantity = 0) {
     this._id = _id
     this.name = name
     this.accessCode = accessCode
     this.prototypeLink = prototypeLink
     this.externalLink = externalLink
     this.tasks = tasks
+    this.quantity = quantity
   }
 }
